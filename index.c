8b93f8d26eb9cd272b0f04c99e77ecc7bb80b13f
Charlie S <152379549+bodied-helium@users.noreply.github.com>
2023-12-04 03:43:17 +0700

    Create index.c

---
+++ index.c
@@ -0,0 +1,10 @@
+// Binary Search in C
+#include <stdio.h>
+
+int binarySearch(int arr[], int low, int high, int key) {
+    // ... (binary search algorithm)
+}
+
+int main() {
+    // ... (main function)
+}
